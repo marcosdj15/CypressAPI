e3fdeacd8d7915409baac8c617414d230fdba7a6
Marcos Rocha <marcosdj2140@gmail.com>
2025-03-20 04:45:16 -0300

    Fix no caso de teste Deletar um produto

--- cypress/E2E/buscar_produto.cy.js
+++ cypress/E2E/buscar_produto.cy.js
@@ -1,28 +1,31 @@
 /// <reference types="cypress"/>
 
-describe('Buscar produto', ()=>{
-
-  it('consultar produto', ()=>{
+describe("Buscar produto", () => {
+  it("consultar produto", () => {
     cy.request({
-        method: 'GET',
-        url: 'https://api.restful-api.dev/objects',
-        
-    })
-     .then((response)=> {
-       expect(response.status).to.equal(200)
-     })
-  })
-  
-  it('consultar produto Id', ()=>{
-    cy.request({
-        method: 'GET',
-        url: 'https://api.restful-api.dev/objects/ff808181923ed5e201927d0559647ef7',
-        
-    })
-     .then((response)=> {
-       expect(response.status).to.equal(200)
-     })
-  })
+      method: "GET",
+      url: "https://api.restful-api.dev/objects",
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    });
+  });
 
- }
-)
+  it("consultar produto Id", () => {
+    cy.request({
+      method: "GET",
+      url: "https://api.restful-api.dev/objects/ff808181923ed5e201927d0559647ef7",
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    });
+  });
+
+  it("consultar produto Id Inexistente", () => {
+    cy.request({
+      method: "GET",
+      url: "https://api.restful-api.dev/objects/888888888888888888",
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.equal(404);
+    });
+  });
+});
